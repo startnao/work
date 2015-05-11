5b51c1a18522514b38e43006898968464f349b09
jgamain <jeanne.gamain@gmail.com>
2015-05-11 14:14:21 +0200

    box sans boucle infinie

---
+++ II/box2.py
@@ -0,0 +1,107 @@
+import cv
+from naoqi import ALProxy
+
+class MyClass(GeneratedClass):
+    def __init__(self):
+        GeneratedClass.__init__(self, False)
+
+    def onLoad(self):
+        self.capture = cv.CaptureFromCAM(0)
+
+    def onUnload(self):
+        pass
+
+    def onInput_onStart(self, p):
+        self.bIsRunning = True
+
+        maxSize = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH) * cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT)
+
+        tts = ALProxy("ALTextToSpeech")
+
+        # Capture first frame to get size
+        frame = cv.QueryFrame(self.capture)
+        frame_size = cv.GetSize(frame)
+        color_image = cv.CreateImage(cv.GetSize(frame), 8, 3)
+        grey_image = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_8U, 1)
+        moving_average = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_32F, 3)
+
+        first = True
+
+        counting = False
+        movement_count = 0
+		
+		i=0
+        while i<50:
+            closest_to_left = cv.GetSize(frame)[0]
+            closest_to_right = cv.GetSize(frame)[1]
+
+            color_image = cv.QueryFrame(self.capture)
+
+            # Smooth to get rid of false positives
+            cv.Smooth(color_image, color_image, cv.CV_GAUSSIAN, 3, 0)
+
+            if first:
+                difference = cv.CloneImage(color_image)
+                temp = cv.CloneImage(color_image)
+                cv.ConvertScale(color_image, moving_average, 1.0, 0.0)
+                first = False
+            else:
+                cv.RunningAvg(color_image, moving_average, 0.020, None)
+
+            # Convert the scale of the moving average.
+            cv.ConvertScale(moving_average, temp, 1.0, 0.0)
+
+            # Minus the current frame from the moving average.
+            cv.AbsDiff(color_image, temp, difference)
+
+            # Convert the image to grayscale.
+            cv.CvtColor(difference, grey_image, cv.CV_RGB2GRAY)
+
+            # Convert the image to black and white.
+            cv.Threshold(grey_image, grey_image, 70, 255, cv.CV_THRESH_BINARY)
+
+            # Dilate and erode to get people blobs
+            cv.Dilate(grey_image, grey_image, None, 18)
+            cv.Erode(grey_image, grey_image, None, 10)
+
+            storage = cv.CreateMemStorage(0)
+            contour = cv.FindContours(grey_image, storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE)
+            points = []
+
+            max_percentage = 0
+
+            while contour:
+                bound_rect = cv.BoundingRect(list(contour))
+                contour = contour.h_next()
+
+                pt1 = (bound_rect[0], bound_rect[1])
+                pt2 = (bound_rect[0] + bound_rect[2], bound_rect[1] + bound_rect[3])
+
+                area = bound_rect[3] * bound_rect[2]
+                percentage = (area / maxSize) * 100
+
+                if percentage > max_percentage:
+                    max_percentage = percentage
+
+                points.append(pt1)
+                points.append(pt2)
+                cv.Rectangle(color_image, pt1, pt2, cv.CV_RGB(255,0,0), 1)
+
+            if not counting and max_percentage > 75:
+                movement_count = 0
+                counting = True
+                tts.say("Starting")
+            elif counting and max_percentage > 20:
+                movement_count += 1
+            elif max_percentage < 20:
+                movement_count = 0
+                counting = False
+                tts.say("Ending")
+
+            if movement_count > 50:
+                self.onStopped()
+                self.bIsRunning = False
+                break
+			i+=1
+    def onInput_onStop(self):
+        self.onUnload()
