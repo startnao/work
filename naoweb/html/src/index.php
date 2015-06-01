d4a68c8871e17bc91d2411148d4fba3bd8cf2a23
philippealeixo <aleixo.philippe@gmail.com>
2015-06-01 10:07:40 +0200

    html

---
+++ naoweb/html/src/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../img/camping.ico">
+    <title>Projet NAO</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="../css/starter-template.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="wrap">
+    <div class="container">
+
+        <div class="text-center">
+
+            <h1 class="modal-header">NAO</h1>
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-menu-up"></span></button>
+            </div>
+            <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-menu-left"></span></button>
+                </div>
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default">Middle</button>
+                </div>
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-menu-right"></span></button>
+                </div>
+            </div>
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-menu-down"></span></button>
+            </div>
+        </div>
