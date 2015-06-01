b5886b5b024d8f975afb6a3a3ff68a0f0f3b78a9
Titouan Galopin <galopintitouan@gmail.com>
2015-06-01 11:50:10 +0200

    Add action execution and JS

---
+++ naoweb/main/static/js/index.js
@@ -0,0 +1,17 @@
+
+var buttons = [
+    { node: document.getElementById('button-up'), action: 'forward' },
+    { node: document.getElementById('button-left'), action: 'left' },
+    { node: document.getElementById('button-right'), action: 'right' },
+    { node: document.getElementById('button-backward'), action: 'backward' }
+];
+
+buttons.forEach(function(button) {
+    button.node.addEventListener('click', function() {
+        var request = qwest.get('/action/' + button.action, { dataType: 'json' });
+
+        request.then(function(response) {
+            console.log(response);
+        });
+    });
+});
--- naoweb/main/views.py
+++ naoweb/main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def index_action(request):
+    return render_to_response('index.html')
+
+def execute_action(request, name):
+    return HttpResponse(json.dumps({"message": name}), content_type="application/json")
